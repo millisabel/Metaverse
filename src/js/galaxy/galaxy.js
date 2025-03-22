1d12a36a3fabcdea7fc6af63cfc7d6a5325c94c7
millisabel <elena.kozlova.job@gmail.com>
2025-03-22 22:23:41 +0100

    feat: added star

--- src/js/galaxy/galaxy.js
+++ src/js/galaxy/galaxy.js
@@ -5,7 +5,7 @@ console.log('Galaxy.js loaded');
 export function initializeScene() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     const hero = document.querySelector('#hero');
     hero.appendChild(renderer.domElement);
