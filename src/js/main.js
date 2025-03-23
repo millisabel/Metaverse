3c98d9d8b5419765bbe26a3b8b9424f27b561c77
millisabel <elena.kozlova.job@gmail.com>
2025-03-23 16:06:19 +0100

    Fix Three.js import path and update HTML to use bundle.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 
-import * as THREE from '../../node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import { initializeScene, createStars, animateStars } from './galaxy/galaxy.js';
 
 console.log('THREE.js version:', THREE.REVISION);
