f467d0eddfc36c459067a5cb09b357c7212999ec
millisabel <elena.kozlova.job@gmail.com>
2025-04-12 00:02:37 +0200

    refactor: enhance logger utility with new color mappings and background options - added additional color mappings for various components and updated formatMessage and log methods to support customizable background and text color options for improved logging flexibility.

--- src/js/utils/logger.js
+++ src/js/utils/logger.js
@@ -1,3 +1,5 @@
+import AnimationObserverCSS from "./animationObserver_CSS";
+
 class Logger {
     static colors = {
         'Glow': '#ff69b4',         // розовый
@@ -6,6 +8,13 @@ class Logger {
         'Stars': '#ffa500',        // оранжевый
         'Controller': '#9370db',   // пурпурный
         'AnimationController': '#9370db', // тот же пурпурный
+        'Roadmap': '#ff4500',      // красный
+        'Navbar': '#00bfff',       // синий
+        'Modal': '#ff1493',        // ярко-розовый
+        'Slider': '#00fa9a',       // светло-зеленый
+        'SocialCards': '#ff6347',  // томатный
+        'Dynamics3D': '#8a2be2',   // синий-фиолетовый
+        'AnimationObserverCSS': '#e2d32b', // синий-фиолетовый
         'default': '#808080'       // серый
     };
 
@@ -13,10 +22,20 @@ class Logger {
         return this.colors[name] || this.colors.default;
     }
 
-    static formatMessage(name, message) {
-        // Убираем [name] из сообщения, если оно там есть
-        const cleanMessage = message.replace(/^\[.*?\]\s*/, '');
-        return `%c[${name}]%c ${cleanMessage}`;
+    static formatMessage(name, message, options = {}) {
+        const cleanMessage = message.replace(/^\[.*?]\s*/, '');
+        if (options.background) {
+            return [
+                `%c[${name}]%c ${cleanMessage}`,
+                `color: ${this.getColor(name)}; font-weight: bold;`,
+                `background-color: ${options.background}; color: ${options.color || 'white'}; padding: 2px 5px; border-radius: 3px;`
+            ];
+        }
+        return [
+            `%c[${name}]%c ${cleanMessage}`,
+            `color: ${this.getColor(name)}; font-weight: bold;`,
+            'color: inherit;'
+        ];
     }
 
     static getStyles(name) {
@@ -27,9 +46,8 @@ class Logger {
         ];
     }
 
-    static log(name = 'System', message, type = 'info') {
-        const formattedMessage = this.formatMessage(name, message);
-        const styles = this.getStyles(name);
+    static log(name = 'System', message, type = 'info', options = {}) {
+        const [formattedMessage, ...styles] = this.formatMessage(name, message, options);
 
         switch (type) {
             case 'error':
@@ -45,7 +63,7 @@ class Logger {
 }
 
 export const createLogger = (name = 'System') => ({
-    log(message, type = 'info') {
-        Logger.log(name, message, type);
+    log(message, type = 'info', options = {}) {
+        Logger.log(name, message, type, options);
     }
 });
