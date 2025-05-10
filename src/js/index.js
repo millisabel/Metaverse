b919979464a5543687141e14af6d8304c53b308c
millisabel <elena.kozlova.job@gmail.com>
2025-05-10 23:31:56 +0200

    refactor: clean up index.js by removing unused Logger functionality and updating copyright year logic, improving code clarity and maintainability

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,3 @@
-// Utils
-import { Logger } from './utils/logger';
-
 // Setup
 import { initHero } from './setup/heroSetup';
 import { initAbout } from "./setup/aboutSetup";
@@ -8,42 +5,12 @@ import { initRoadmap } from './setup/roadmapSetup';
 import { initDynamics } from './setup/dynamicsSetup';
 import { initVRMarket } from './setup/vrMarketSetup';
 import { initExplore } from './setup/exploreSetup';
-
+import { initSocial } from './setup/socialSetup';
 // Common components
 import { initNavbar } from './components/common/navbar';
 import { initSlider } from './components/common/slider';
 import initModal from './components/common/modal';
-
-// 3D components
-import { initSocialCards } from './components/three/socialCards';
-
-if (process.env.NODE_ENV === 'development') {
-    Logger.enableGlobalLogging(false);
-    Logger.disableLoggerFor('HeroSetup');
-    Logger.disableLoggerFor('AboutSetup');
-    Logger.disableLoggerFor('RoadmapSetup');
-    Logger.disableLoggerFor('DynamicsSetup');
-    Logger.disableLoggerFor('ThreeDContainerManager');
-    Logger.disableLoggerFor('CameraController');
-    Logger.disableLoggerFor('AnimationController_3D');
-    Logger.disableLoggerFor('Utils');
-    Logger.disableLoggerFor('Stars');
-    Logger.disableLoggerFor('GalacticCloud');
-    Logger.disableLoggerFor('Constellation');
-    Logger.disableLoggerFor('Glow');
-    Logger.disableLoggerFor('SingleGlow');
-    Logger.disableLoggerFor('Roadmap');
-    Logger.disableLoggerFor('Dynamics3D');
-    Logger.disableLoggerFor('AnimationObserverCSS');
-    Logger.disableLoggerFor('VRMarketSetup');
-    Logger.disableLoggerFor('CharacterFloatingBadge');
-    Logger.disableLoggerFor('ExploreSetup');
-    Logger.disableLoggerFor('ExploreScene');     
-    Logger.disableLoggerFor('AnimationController_3D');
-    Logger.disableLoggerFor('AnimationController');
-} else {
-    Logger.disableGlobalLogging();
-}
+import { updateCopyrightYear } from './utils/utils';
 
 document.addEventListener('DOMContentLoaded', () => {
     initHero();
@@ -52,15 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
     initDynamics();
     initVRMarket();
     initExplore();
-    initSocialCards();
+    initSocial();
 
     // Initialize UI components
     initNavbar();
     initModal();
     initSlider();
-    
-    // Update copyright year
-    document.getElementById('currentYear').textContent = new Date().getFullYear();
+    updateCopyrightYear('[data-year="currentYear"]');
 });
-
-
