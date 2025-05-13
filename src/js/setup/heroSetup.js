79fc3e2dcb4c3190c4387ddef115d196693d24a2
millisabel <elena.kozlova.job@gmail.com>
2025-05-13 21:17:52 +0200

    feat: enhance HeroSetup class with detailed 3D object configuration, including core, plane, and animation properties for improved visual effects and responsiveness

--- src/js/setup/heroSetup.js
+++ src/js/setup/heroSetup.js
@@ -5,7 +5,20 @@ import { Universal3DSection } from '../utilsThreeD/Universal3DSection';
 import { Stars } from "../components/three/stars";
 import { GalacticCloud } from '../components/three/galactic';
 
+/**
+ * SECTION_ID
+ * @description The section id for the hero section
+ * @type {string}
+ */
 const SECTION_ID = 'hero';
+
+/**
+ * HERO_3D_OBJECTS
+ * @description The 3D objects for the hero section
+ * @type {Object}
+ * @property {Object} STARS - The stars
+ * @property {Object} GALACTIC - The galactic cloud
+ */
 const HERO_3D_OBJECTS = {
     STARS: {
         classRef: Stars,
@@ -44,10 +57,34 @@ const HERO_3D_OBJECTS = {
             far: 1000,
             position: { x: 0, y: 5, z: 15 },
             rotation: true,
-        }
+        },
+        core: {
+            size: 2,
+        },
+        plane: {
+            size: isMobile() ? 4 : 15,
+            opacity: 0.6,
+            transparent: true,
+        },
+        bloom: {
+            strength: 0.5, 
+            radius: isMobile() ? 0.1 : 2,
+            threshold: 0.2, 
+        },
+        animation: {
+            minScale: 1,
+            corePulse: 5,
+        },
     }
 };
 
+/**
+ * HERO_3D_OBJECTS
+ * @description The 3D objects for the hero section
+ * @type {Object}
+ * @property {Object} STARS - The stars
+ * @property {Object} GALACTIC - The galactic cloud
+ */
 export class HeroSetup extends Universal3DSection {
     constructor() {
       super(SECTION_ID, HERO_3D_OBJECTS);
