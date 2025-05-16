385e24be3689d0c5b06835a694c20aece3f21dbd
millisabel <elena.kozlova.job@gmail.com>
2025-05-17 00:51:02 +0200

    refactor: enhance Universal3DSection class by adding detailed JSDoc comments for improved documentation and clarity; implement lazy loading of 3D controllers with viewport detection for better performance and resource management

--- src/js/utilsThreeD/Universal3DSection.js
+++ src/js/utilsThreeD/Universal3DSection.js
@@ -1,6 +1,13 @@
 import { BaseSetup } from '../utilsThreeD/baseSetup';
 import { createLogger } from '../utils/logger';
 
+/**
+ * @description Universal 3D Section
+ * @extends {BaseSetup}
+ * @param {string} containerId - The ID of the container element
+ * @param {Object} objects3DConfig - The configuration for the 3D objects
+ * @returns {Universal3DSection}
+ */
 export class Universal3DSection extends BaseSetup {
     constructor(containerId, objects3DConfig) {
         super(containerId);
@@ -8,40 +15,100 @@ export class Universal3DSection extends BaseSetup {
         this.name = `(Universal3DSection) ⬅ ${this.constructor.name}`;
         this.logger = createLogger(this.name);
 
-        
         this.objects3D = objects3DConfig;
         this.controllers = {};
+        this._controllersCreated = false;
+        this._3dContainers = {};
 
-        for (const [key, params] of Object.entries(this.objects3D)) {
-            this.controllers[key] = this.create3DController(key, params);
-        }
-  
+        this._initLazyObserver();
+
         this.logger.log({
-          functionName: 'constructor',
-          conditions: ['init'],
-          customData: {
-              this: this
-          }
+            functionName: 'constructor',
+            conditions: ['init'],
+            customData: { this: this }
         });
     }
-  
-    async setupScene() {
 
+    /**
+     * @description Initialize the lazy observer
+     * @returns {void}
+     */
+    _initLazyObserver() {
+        this._lazyObserver = new IntersectionObserver(async entries => {
+            for (const entry of entries) {
+                if (entry.isIntersecting) {
+                    await this._onEnterViewport();
+                } else {
+                    this._onExitViewport();
+                }
+            }
+        }, { threshold: 0.1, rootMargin: '100px' });
+
+        this._lazyObserver.observe(this.container);
+    }
+
+    /**
+     * @description On enter viewport
+     * @returns {void}
+     */
+    async _onEnterViewport() {
+        if (!this._controllersCreated) {
+            for (const [key, params] of Object.entries(this.objects3D)) {
+                if (!this._3dContainers[key]) {
+                    this._3dContainers[key] = this.createContainer(params.containerName || key, params.zIndex || 1);
+                }
+                if (!this.controllers[key]) {
+                    this.controllers[key] = new params.classRef(this._3dContainers[key], {
+                        ...params,
+                        camera: params.camera
+                    });
+                }
+            }
+            this._controllersCreated = true;
+            await this.setupScene();
+        } else {
+            await this.setupScene();
+        }
+    }
+
+    /**
+     * @description On exit viewport
+     * @returns {void}
+     */
+    _onExitViewport() {
+        if (this._controllersCreated) {
+            for (const controller of Object.values(this.controllers)) {
+                if (controller && typeof controller.cleanup === 'function') {
+                    controller.cleanup();
+                }
+            }
+        }
+    }
+
+    /**
+     * @description Setup the scene
+     * @returns {void}
+     */
+    async setupScene() {
         this.logger.log({
             functionName: 'setupScene',
             conditions: ['init'],
-            customData: {
-                this: this
-            }
+            customData: { this: this }
         });
 
         for (const controller of Object.values(this.controllers)) {
-          if (controller && typeof controller.init === 'function') {
-            await controller.init();
-          }
+            if (controller && typeof controller.init === 'function') {
+                await controller.init();
+            }
         }
-      }
+    }
 
+    /**
+     * @description Create a 3D controller
+     * @param {string} type - The type of the 3D object
+     * @param {Object} params - The parameters for the 3D object
+     * @returns {Object}
+     */
     create3DController(type, params) {
         const containerName = params.containerName || type;
         const zIndex = params.zIndex || 1;
@@ -68,6 +135,10 @@ export class Universal3DSection extends BaseSetup {
         });
     }
 
+    /**
+     * @description On resize
+     * @returns {void}
+     */
     onResize() {
         Object.values(this.controllers).forEach(ctrl => {
             if (ctrl && typeof ctrl.onResize === 'function') {
@@ -76,17 +147,25 @@ export class Universal3DSection extends BaseSetup {
         });
     }
 
+    /**
+     * @description Update the controllers
+     * @returns {void}
+     */
     update() {
-    for (const controller of Object.values(this.controllers)) {
+        for (const controller of Object.values(this.controllers)) {
             if (controller && typeof controller.update === 'function') {
                 controller.update();
             }
         }
     }
 
+    /**
+     * @description Cleanup the controllers
+     * @returns {void}
+     */
     cleanup() {
-    for (const controller of Object.values(this.controllers)) {
-        let logMessage = `starting cleanup in ${this.constructor.name}\n`;
+        for (const controller of Object.values(this.controllers)) {
+            let logMessage = `starting cleanup in ${this.constructor.name}\n`;
 
         if (controller && typeof controller.cleanup === 'function') {
             logMessage += `controller: ${this.controllers}\n`;
