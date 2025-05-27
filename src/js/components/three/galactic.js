b61a34cdffc2397338172648808f0cd84d499fb0
millisabel <elena.kozlova.job@gmail.com>
2025-05-28 00:30:50 +0200

    refactor: enhance GalacticCloud with detailed orbit options and responsive configurations for improved animation dynamics and code clarity

--- src/js/components/three/galactic.js
+++ src/js/components/three/galactic.js
@@ -11,10 +11,33 @@ import fragmentShader from '../../shaders/galacticCore.frag';
 const galacticTexture = './assets/images/galaxy-texture.png';
 
 /**
- * @description The default options for the galactic cloud
+ * @description The default options for galactic cloud
  * @type {Object}
- * @property {number} core.size - The size of the core
- * @property {number} core.segments - The segments of the core
+ * @property {Object} orbit - The orbit options
+ * @property {Object} core - The core options
+ * @property {Object} plane - The plane options
+ * @property {Object} orbit.rotation - The rotation options x, y, z
+ * @property {Object} orbit.initialOffset - The initial offset options x, y, z
+ * @property {Object} orbit.phase - The phase options x, y, z
+ * @property {Object} orbit.amplitude - The amplitude options x, y, z
+ * @property {Object} orbit.scale - The scale options min, max
+ * @property {Number} orbit.speedPulse - The speed pulse range 0-1
+ * @property {Number} orbit.speedMove - The speed move range 0-1
+ * @property {Number} core.size - The size options
+ * @property {Number} core.segments - The segments options
+ * @property {Object} core.shader - The shader options
+ * @property {Number} core.shader.opacity - The opacity options range 0-1
+ * @property {Array} core.shader.color - The color options
+ * @property {Number} core.shader.transitionRadius - The transition radius options range 0.01-1.0
+ * @property {Object} core.shader.pulse - The pulse options
+ * @property {Number} core.shader.pulse.amplitudeCore - The amplitude core options range 0-1
+ * @property {Number} core.shader.pulse.amplitudeEdge - The amplitude edge options  range 0.01 – 10.0
+ * @property {Number} core.shader.pulse.speedCore - The speed core options range 0-1
+ * @property {Number} core.shader.pulse.speedEdge - The speed edge options 
+ * @property {Number} plane.size - The size options
+ * @property {Number} plane.opacity - The opacity options range 0-1
+ * @property {Boolean} plane.transparent - The transparent options
+ * @property {Number} plane.rotationSpeed - The rotation speed options 
  */
 
 const DEFAULT_OPTIONS = {
@@ -25,8 +48,8 @@ const DEFAULT_OPTIONS = {
             z: 0,
         },
         initialOffset: {
-            x: 2,
-            y: 0,
+            x: isMobile() ? 0 : 2,
+            y: isMobile() ? -2 : 0,
             z: -2,
         },
         phase: {
@@ -37,24 +60,25 @@ const DEFAULT_OPTIONS = {
         amplitude: {
             x: { min: -2, max: 2 },
             y: { min: -2, max: 1 },
-            z: { min: -2, max: -8 },
+            z: { min: -2, max: -5 },
         },
         scale: {
             min: 0.8,
-            max: 2,
+            max: 3,
         },
-        speedPulse: 0.05,
+        speedPulse: 0.005,
+        speedMove: isMobile() ? 0 : 0.01,
     },
     core: {
-        size: 2.5,               
+        size: isMobile() ? 2 : 2.5,               
         segments: 4,           
         shader: {               
-            opacity: 0.5,
+            opacity: 0.5, 
             color: {
-                core: [1.0, 1.0, 1.0],
+                core: [1.0, 1.0, 1.0], 
                 edge: [0.8, 0.4, 1.0],
             },
-            transitionRadius: 0.3, 
+            transitionRadius: 0.3,
             pulse: {
                 amplitudeCore: 0.10, 
                 amplitudeEdge: 0.50, 
@@ -69,13 +93,26 @@ const DEFAULT_OPTIONS = {
         transparent: false,
         rotationSpeed: 0.0005,
     },
+    responsive: {
+        orbit: {
+            initialOffset: {
+                'x': 'isMobile() ? 0 : 2',
+                'y': 'isMobile() ? -2 : 0',
+            },
+            speedMove: 'isMobile() ? 0 : 0.01',
+        },
+        core: {
+            size: 'isMobile() ? 2 : 2.5',
+        },
+        plane: {
+            size: 'isMobile() ? 2 : 3',
+        },
+    },
 };
 
 /**
  * @description The galactic cloud
  * @type {Object}
- * @property {Object} core - The core of the galactic cloud
- * @property {Object} plane - The plane of the galactic cloud
  */
 export class GalacticCloud extends Object_3D_Observer_Controller {
     constructor(container, options = {}) {
@@ -87,8 +124,6 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
         this.galaxyCore = null;
         this.galaxyPlane = null;
         this.shaderController = null;
-
-        this._orbitPhase = this.options.orbit.phase;
     }
 
     /**
@@ -100,11 +135,11 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
         await this._createGalaxyCore();
         await this._createGalaxyPlane();
 
+        this._setObjectPosition(this.galaxyCore, this.options.orbit.initialOffset);
+        this._setObjectPosition(this.galaxyPlane, this.options.orbit.initialOffset);
+
         this._setOrbitRotation(this.galaxyCore);
         this._setOrbitRotation(this.galaxyPlane);
-
-        this._setOrbitPosition(this.galaxyCore);
-        this._setOrbitPosition(this.galaxyPlane);
     }
 
     /**
@@ -115,7 +150,8 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
     update() {
         const time = performance.now() * 0.001;
 
-        this._updateScalePulse(time);
+        this._updateObjectPosition(time);
+        this._updateObjectPulse(time);
         this._updateCorePulse(time);
         this._updatePlaneRotation();
         super.update();
@@ -127,19 +163,9 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
      * @returns {Promise<void>}
      */
     onResize() {
-        if (!this.renderer || !this.camera) return;
-
+        if (!this.renderer || !this.cameraController.camera) return;
+        
         super.onResize();
-        this._updateCameraOrbit(0); 
-
-        if (this.galaxyPlane) {
-            const planeSize = this.options.plane.size;
-            this.galaxyPlane.scale.set(planeSize/8, planeSize/8, planeSize/8);
-        }
-
-        if (this.galaxyCore) {
-            this.galaxyCore.material.uniforms.resolution.value.set(this.container.clientWidth, this.container.clientHeight);
-        }
     }
 
     /**
@@ -148,9 +174,45 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
      * @returns {Promise<void>}
      */
     cleanup() {
-        let message = `starting cleanup in ${this.constructor.name}\n`;
+        this.logMessage += 
+            `----------------------------------------------------------\n` +
+            `${this.constructor.name} starting cleanup in GalacticCloud\n` + 
+            `----------------------------------------------------------\n`;
+        if (this.galaxyCore) {
+            if (this.scene) this.scene.remove(this.galaxyCore);
+            if (this.galaxyCore.geometry) this.galaxyCore.geometry.dispose();
+            if (this.galaxyCore.material) this.galaxyCore.material.dispose();
+            this.galaxyCore = null;
+        }
+    
+        if (this.galaxyPlane) {
+            if (this.scene) this.scene.remove(this.galaxyPlane);
+            if (this.galaxyPlane.geometry) this.galaxyPlane.geometry.dispose();
+            if (this.galaxyPlane.material) {
+                if (this.galaxyPlane.material.map) {
+                    this.galaxyPlane.material.map.dispose();
+                }
+                this.galaxyPlane.material.dispose();
+            }
+            this.galaxyPlane = null;
+        }
+    
+        if (this.shaderController && typeof this.shaderController.dispose === 'function') {
+            this.shaderController.dispose();
+        }
+        this.shaderController = null;
 
-        super.cleanup(message);
+        this.logMessage += 
+            `${this.constructor.name} this.galaxyCore ${this.galaxyCore}\n` + 
+            `${this.constructor.name} this.galaxyPlane ${this.galaxyPlane}\n` + 
+            `${this.constructor.name} this.shaderController ${this.shaderController}\n` + 
+            `${this.constructor.name} cleanup completed\n`;
+
+        super.cleanup();
+
+        this.logMessage += 
+        `----------------------------------------------------------\n` +
+            `${this.constructor.name} cleanup completed\n`;
     }
 
     /**
@@ -225,16 +287,69 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
     }
 
     /**
+     * @description Gets the position of the object based on the orbit options
+     * @param {number} t
+     * @returns {Object}
+     * @protected
+     */
+    _getPosition(t) {
+        const { initialOffset, amplitude, phase, speedMove } = this.options.orbit;
+    
+        const f = (mult, phase) => (time) =>
+            Math.sin(time * speedMove * mult + phase) - Math.sin(phase); // компенсация начального смещения
+    
+        const fC = (mult, phase) => (time) =>
+            Math.cos(time * speedMove * mult + phase) - Math.cos(phase);
+    
+        return {
+            x: initialOffset.x +
+                this._mapAmplitude(
+                    amplitude.x,
+                    f(1.3, phase.x)(t) + 0.5 * fC(4.2, phase.x)(t)
+                ),
+            y: initialOffset.y +
+                this._mapAmplitude(
+                    amplitude.y,
+                    f(1.0, phase.y)(t)
+                ),
+            z: initialOffset.z +
+                this._mapAmplitude(
+                    amplitude.z,
+                    f(1.3, phase.z)(t) + 0.5 * fC(3.3, phase.z)(t)
+                ),
+        };
+    }
+
+    /**
      * @description Sets the position of the object based on the orbit options
      * @param {THREE.Object3D} obj - The object to set the position
      * @returns {void}
      * @protected
      */
-    _setOrbitPosition(obj) {
-        obj.position.x = this.options.orbit.initialOffset.x;
-        obj.position.y = this.options.orbit.initialOffset.y;
-        obj.position.z = this.options.orbit.initialOffset.z;
+    _setObjectPosition(obj, position) {
+        if (obj && position) {
+            obj.position.x = position.x;
+            obj.position.y = position.y;
+            obj.position.z = position.z;
+        }
       }
+
+    /**
+     * @description Updates the orbit position
+     * @param {number} t
+     * @returns {void}
+     * @protected
+     */
+    _updateObjectPosition(t) {
+        const position = this._getPosition(t);
+
+        if (this.galaxyCore) {
+            this._setObjectPosition(this.galaxyCore, position); 
+        }
+        if (this.galaxyPlane) {
+            this._setObjectPosition(this.galaxyPlane, position);
+        }
+    }
 
     /**
      * @description Sets the rotation of the object based on the orbit options
@@ -243,9 +358,11 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
      * @protected
      */
     _setOrbitRotation(obj) {
-        obj.rotation.x = this.options.orbit.rotation.x;
-        obj.rotation.y = this.options.orbit.rotation.y;
-        obj.rotation.z = this.options.orbit.rotation.z;
+        if (obj && this.options.orbit.rotation) {   
+            obj.rotation.x = this.options.orbit.rotation.x;
+            obj.rotation.y = this.options.orbit.rotation.y;
+            obj.rotation.z = this.options.orbit.rotation.z;
+        }
     }  
 
     /**
@@ -272,6 +389,39 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
     }
 
     /**
+     * @description Updates the scale pulse
+     * @param {number} t
+     * @returns {void}
+     * @protected
+     */
+    _updateObjectPulse(t) {
+        const speedPulse = this.options.orbit.speedPulse || 0;
+        const scaleOpts = this.options.orbit.scale;
+        const phase = this.options.orbit.phase;
+        let pulse = 1;
+        if (speedPulse > 0) {
+            pulse = this._calcPulseScale(t * speedPulse, phase, scaleOpts);
+        }
+        if (this.galaxyCore) {
+            this.galaxyCore.scale.set(pulse, pulse, pulse);
+        }
+        if (this.galaxyPlane) {
+            this.galaxyPlane.scale.set(pulse * 0.8, pulse * 0.8, pulse * 0.8);
+        }
+    }
+    
+    /**
+     * @description Maps the amplitude
+     * @param {object} ampl
+     * @param {number} value
+     * @returns {number}
+     * @protected
+     */
+    _mapAmplitude (ampl, value) {
+        return ((ampl.max - ampl.min) / 2) * value + (ampl.max + ampl.min) / 2;
+    }
+
+    /**
      * @description Calculates the pulse scale  
      * @param {number} time
      * @param {object} phase
@@ -285,36 +435,22 @@ export class GalacticCloud extends Object_3D_Observer_Controller {
         if (speed === 0) {
             return (scaleOpts.min + scaleOpts.max) / 2;
         }
+    
         const t = time * (speed > 0 ? speed : 1);
+    
         const base = Math.sin(t * 2 + phase.x) * 0.5 + 0.5;
         const extra = Math.sin(t * 3.1 + phase.y) * 0.15;
-        let scale = scaleOpts.min + (scaleOpts.max - scaleOpts.min) * (base + extra)
+    
+        const base0 = Math.sin(phase.x) * 0.5 + 0.5;
+        const extra0 = Math.sin(phase.y) * 0.15;
+    
+        const delta = (base + extra) - (base0 + extra0);
+    
+        let scale = 2.5 + (scaleOpts.max - scaleOpts.min) * delta;
+    
         scale = Math.max(scaleOpts.min, Math.min(scaleOpts.max, scale));
         return scale;
     }
-
-    /**
-     * @description Updates the scale pulse
-     * @param {number} t
-     * @returns {void}
-     * @protected
-     */
-    _updateScalePulse(t) {
-        const speedPulse = this.options.orbit.speedPulse || 0;
-        const scaleOpts = this.options.orbit.scale;
-        const phase = this._orbitPhase;
-        let pulse = 1;
-        if (speedPulse > 0) {
-            pulse = this._calcPulseScale(t * speedPulse, phase, scaleOpts);
-        }
-        if (this.galaxyCore) {
-            this.galaxyCore.scale.set(pulse, pulse, pulse);
-        }
-        if (this.galaxyPlane) {
-            this.galaxyPlane.scale.set(pulse * 0.8, pulse * 0.8, pulse * 0.8);
-        }
-    }
 }
-
 
 
